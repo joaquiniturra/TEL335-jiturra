9b6195cf959b316577072a4ab20086d7b900c360
joaquiniturra <joaquiniturra9@gmail.com>
2025-05-08 20:03:14 -0400

    First attempt

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import kotlinx.serialization.Serializable
+import org.slf4j.event.*
+
+fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html`
+        staticResources("/static", "static")
+    }
+}
